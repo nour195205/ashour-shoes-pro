d631656e21d1d729d249ebde70d95284461017aa
nour195205 <152740963+nour195205@users.noreply.github.com>
2025-03-31 21:46:22 +0200

    second commit

--- shop/urls.py
+++ shop/urls.py
@@ -3,14 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.products, name='products'),
-    path('filter', views.filter_products, name='filter'),
-    path('product/<int:product_id>/', views.product_details, name='product_details'),
+    path('products/<int:product_id>/', views.product_details, name='product_details'),
+    path('filter/', views.filter_products, name='filter_products'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.view_cart, name='view_cart'),
     path('checkout/', views.checkout, name='checkout'),
-    path('orders/', views.view_orders, name='orders'),
     path('contactus/', views.contact_us, name='contact_us'),
+    path('orders/', views.view_orders, name='orders'),
+    path('custom-order/', views.custom_order, name='custom_order'),  # رابط صفحة الطلب
+    path('submit-custom-order/', views.submit_custom_order, name='submit_custom_order'),  # رابط إرسال الطلب
 ]
